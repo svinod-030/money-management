3c9a7509b35a4ffe3d65213c055ca67140a4220e
vinod_sigadana_x <vinod_sigadana_x@telkomsel.co.id>
2022-02-16 11:00:14 +0530

    [Vinod] | Add basic functionality as editable table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Grid from './components/grid/Grid'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Money Management</p>
       </header>
+      <body className="Sheet">
+        <Grid/>
+      </body>
     </div>
   );
 }
